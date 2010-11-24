4549183b0e2e5f8dff5b036c53a0f0ce9f7ff6e2
michaelmure <michaelmure@10626965-498c-4fa7-af39-c179cd213045>
2010-11-24 10:24:22 +0000

    add Suit class
    git-svn-id: https://belotus.svn.sourceforge.net/svnroot/belotus/trunk@20 10626965-498c-4fa7-af39-c179cd213045

---
+++ src/BelotusCommon/Suit.h
@@ -0,0 +1,41 @@
+/* Belotus
+ *
+ * Suit.h
+ * Copyright (C) 2010 Schneider Julien
+ * Copyright (C) 2010 Michael Muré <batolettre@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef SUIT_H
+#define SUIT_H
+
+#include "../BelotusCommon/Base.h"
+
+typedef enum {
+    Heart = 0,
+    Diamond,
+    Club,
+    Spade} SuitType;
+
+class Suit : public Base
+{
+public:
+    Suit(SuitType type);
+    SuitType getType() const;
+private:
+    std::ostream& PrintOn(std::ostream&) const;
+    SuitType type;
+};
+
+#endif // SUIT_H
