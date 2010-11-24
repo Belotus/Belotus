e43218f96d565dc3709d55af60da0a1c08841db3
michaelmure <michaelmure@10626965-498c-4fa7-af39-c179cd213045>
2010-11-24 10:25:10 +0000

    add Value class
    git-svn-id: https://belotus.svn.sourceforge.net/svnroot/belotus/trunk@21 10626965-498c-4fa7-af39-c179cd213045

---
+++ src/BelotusCommon/Value.cpp
@@ -0,0 +1,58 @@
+/* Belotus
+ *
+ * Value.cpp
+ * Copyright (C) 2010 Schneider Julien
+ * Copyright (C) 2010 Michael Muré <batolettre@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "Value.h"
+#include <string>
+#include <iostream>
+
+Value::Value(CardValue value) :
+        value(value)
+{
+}
+
+CardValue Value::getType() const {
+    return this->value;
+}
+
+std::ostream& Value::PrintOn(std::ostream& os) const {
+    switch(this->value) {
+    case SEVEN:
+    case EIGHT:
+    case NINE:
+    case TEN:
+        return os << "Value: " << this->value;
+        break;
+    case JACKS:
+        return os << "Value: Jacks";
+        break;
+    case QUEEN:
+        return os << "Value: Queen";
+        break;
+    case KING:
+        return os << "Value: King";
+        break;
+    case ACE:
+        return os << "Value: Ace";
+        break;
+    default:
+        return os << "Error: This Value doens't exist.";
+        break;
+    }
+}
---
+++ src/BelotusCommon/Value.h
@@ -0,0 +1,45 @@
+/* Belotus
+ *
+ * Value.h
+ * Copyright (C) 2010 Schneider Julien
+ * Copyright (C) 2010 Michael Muré <batolettre@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef VALUE_H
+#define VALUE_H
+
+#include "../BelotusCommon/Base.h"
+
+typedef enum {
+    SEVEN = 7,
+    EIGHT,
+    NINE,
+    TEN,
+    JACKS,
+    QUEEN,
+    KING,
+    ACE} CardValue;
+
+class Value : public Base
+{
+public:
+    Value(CardValue value);
+    CardValue getType() const;
+private:
+    std::ostream& PrintOn(std::ostream&) const;
+    CardValue value;
+};
+
+#endif // VALUE_H
